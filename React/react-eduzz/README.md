fea100b7e674f638465f9ba684de50c65d0106f0
whazys <santos_rafa1973@hotmail.com>
2021-09-27 16:33:00 -0300

    Main

---
+++ React/react-eduzz/README.md
@@ -0,0 +1,4 @@
+Introdução ao React
+
+- 1 - Conceitos / Configurações do ambiente de estudos
+- 2 - Rendererização de Elementos
---
+++ React/react-eduzz/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+function App() {
+  return <div className="App">Começando Aulas</div>
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
