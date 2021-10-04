dd4100f53713306f920ee5fe9f5a5d54f9cb7b3d
whazys <santos_rafa1973@hotmail.com>
2021-10-04 18:55:56 -0300

    Main

---
+++ React Adv/advanced-reactjs/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Twitter from './Twitter'
+
+class App extends Component {
+  render() {
+    const posts = [
+      {
+        title: 'xpto',
+        description: 'foo'
+      },
+      {
+        title: 'bar',
+        description: 'foo'
+      }
+    ]
+
+    return <Twitter posts={posts} />
+  }
+}
+
+export default App
