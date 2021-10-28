5cf46ef4099d0deac01e0c8359d07a40f1a2a91a
whazys <santos_rafa1973@hotmail.com>
2021-10-27 23:23:19 -0300

    Node-Main

--- NodeJs/live-01/src/index.ts
+++ NodeJs/live-01/src/index.ts
@@ -1,11 +1,18 @@
-import express, {Request, Response, NextFunction} from "express";
+import express from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
 
-app.get('./status', (req: express.Request, res: express.Response, next) => {
-  res.status(200).send({foo: 'bar'});
-})
+// Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000,() => {
-  console.log('Aplicação em execução')
-})
+// Configurações de Rotas
+app.use(statusRoute);
+app.use(usersRoute);
+
+// Inicialização do servidor
+app.listen(3000, () => {
+    console.log('Aplicação executando na porta 3000!');
+});
